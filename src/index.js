24e14e9a60c2810bebbf91f0a76e3a3c96b6e0d3
Igor Fernandes <igorrfc10@gmail.com>
2017-04-24 14:18:06 -0300

    Update the src/index file
    adding the store creation and its sharing with the App's main component.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { logger } from 'redux-logger';
+
+import reducers from './reducers';
+import App from './components/App';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const middlewares = applyMiddleware(logger);
+
+let store = createStore(
+  reducers,
+  composeEnhancers(middlewares),
+);
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
